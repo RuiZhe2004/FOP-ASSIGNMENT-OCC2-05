056b8655850061fc879493ba93dbfeb072a7c22e
szeyu <szeyusim@gmail.com>
2023-10-21 21:48:11 +0800

    delete week01 and recreate

--- 23005023/src/Week01/L01Q01.java
+++ 23005023/src/Week01/L01Q01.java
@@ -1,28 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Week01;
 
 /**
  *
- * @author Badrul Anuar
- * 
- *  a) Make sure your project name is your matrix number. Please rename it.
- *  b) Create new packages with WeekXX - (i.e XX is the lab number)
- *  c) Make sure your new packages start with WeekXX 
- *  d) Start your lab with with the following format LXXQYY - (XX is your lab session number and YY is the question number). alway start with 01 and continues with 02, 03, ...
- *  e) Delete this sample packages.
- *  f) Commit and push once completed
+ * @author szeyu
  */
 public class L01Q01 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
 }
