deb0704efcea1f67a7f1d1090872c98eaf353bcf
szeyu <szeyusim@gmail.com>
2023-10-21 22:15:01 +0800

    done L01Q03 and change the public to private in l01q02

---
+++ 23005023/src/Week01/L01Q03.java
@@ -0,0 +1,20 @@
+
+package Week01;
+
+/**
+ *
+ * @author szeyu
+ */
+public class L01Q03 {
+    public static void main(String[] args){
+        System.out.println("*********          ***            *             *");
+        System.out.println("*       *        *     *         ***           * *");
+        System.out.println("*       *       *       *       *****         *   *");
+        System.out.println("*       *       *       *         *          *     *");
+        System.out.println("*       *       *       *         *         *       *");
+        System.out.println("*       *       *       *         *          *     *");
+        System.out.println("*       *       *       *         *           *   *");
+        System.out.println("*       *        *     *          *            * *");
+        System.out.println("*********          ***            *             *");
+    }
+}
