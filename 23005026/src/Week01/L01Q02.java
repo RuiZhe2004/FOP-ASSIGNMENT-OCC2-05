473702484d999407b1dbbcd3159f2fd5eae465b7
Xin-overclocked <147032990+Xin-overclocked@users.noreply.github.com>
2023-10-22 13:00:12 +0800

    Completed Lab W01 by Low Jia Xin

--- 23005026/src/Week01/L01Q02.java
+++ 23005026/src/Week01/L01Q02.java
@@ -10,14 +10,15 @@ package Week01;
  */
 public class L01Q02 {
         public static void main(String[] args) {
-                System.out.println("My Name: LOW JIA XIN");
-                System.out.println("Matric Number: 23005026");
-                System.out.println("Address: Bayan Lepas, Pulau Pinang");
-                System.out.println("Email: 23005026@siswa.um.edu.my");
-                System.out.println("Contact: +6017 - 293 0933\n\n");
-                for (int i=0; i< 40; i++) {
-                    System.out.print("-");
-                }
-                System.out.println();
+            System.out.printf("%15s%s\n","","My Profile");
+            for (int i=0; i< 40; i++) {
+                System.out.print("-");
+            }
+            System.out.println();            
+            System.out.println("My Name: LOW JIA XIN");
+            System.out.println("Matric Number: 23005026");
+            System.out.println("Address: Bayan Lepas, Pulau Pinang");
+            System.out.println("Email: 23005026@siswa.um.edu.my");
+            System.out.println("Contact: +6017 - 293 0933\n\n");
     }
 }
