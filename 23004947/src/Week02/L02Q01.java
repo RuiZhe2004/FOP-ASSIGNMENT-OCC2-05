5f494c39bbb4096af88be23a33525d29a8be2cab
ooiru <ooiru@ReztonRZ>
2023-10-24 21:12:08 +0800

    Completed Lab W2 Q1 by Rui Zhe Ooi

--- 23004947/src/Week02/L02Q01.java
+++ 23004947/src/Week02/L02Q01.java
@@ -1,6 +1,12 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package Week02;
-
+/**
+ *
+ * @author rz_rexton
+ */
 import java.util.Scanner;
 public class L02Q01 {
     public static void main(String[] args) {
--- 23004947/src/Week02/L02Q02.java
+++ 23004947/src/Week02/L02Q02.java
@@ -1,6 +1,12 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package Week02;
-
+/**
+ *
+ * @author rz_rexton
+ */
 import java.util.Scanner;
 public class L02Q02 {
     public static void main(String[] args) {
