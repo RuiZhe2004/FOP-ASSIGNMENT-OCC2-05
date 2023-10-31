e855de45b108dfe76d64c302730702622732268c
szeyu <szeyusim@gmail.com>
2023-10-31 13:39:17 +0800

    done L03Q04

---
+++ 23005023/src/Week03/L03Q04.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Week03;
+
+/**
+ *
+ * @author szeyu
+ */
+import java.util.Random;
+public class L03Q04 {
+    public static void main(String[] args){
+        Random random = new Random();
+        
+        int player1Score = 0;
+        int player2Score = 0;
+        
+        player1Score += random.nextInt(6)+1;
+        player1Score += random.nextInt(6)+1;
+
+        player2Score += random.nextInt(6)+1;
+        player2Score += random.nextInt(6)+1;
+        
+        System.out.println("Player 1 Score : " + player1Score);
+        System.out.println("Player 2 Score : " + player2Score);
+        
+        if (player1Score > player2Score)
+            System.out.println("Player 1 win");
+        else if (player2Score > player1Score)
+            System.out.println("Player 2 win");
+        else
+            System.out.println("Tie");
+    }
+}
