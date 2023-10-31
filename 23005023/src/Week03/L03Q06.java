825ba29d953fe1924d3f6c4245ba6b4ce81d3c80
szeyu <szeyusim@gmail.com>
2023-10-31 13:39:51 +0800

    done L03Q06

---
+++ 23005023/src/Week03/L03Q06.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Week03;
+
+/**
+ *
+ * @author szeyu
+ */
+import java.util.Scanner;
+import java.lang.Math;
+public class L03Q06 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Radius : ");
+        int radius = input.nextInt();
+        
+        System.out.print("x : ");
+        int x = input.nextInt();
+        System.out.print("y : ");
+        int y = input.nextInt();
+        
+        double distance = Math.sqrt(x*x + y*y);
+        
+        System.out.printf("Distance : %.2f%n", distance);
+        
+        if (distance < (double)radius)
+            System.out.println("Inside the circle");
+        else if (distance > (double)radius)
+            System.out.println("Outside the circle");
+        else
+            System.out.println("on the circle");
+    }
+}
