32ecc42402f16d1a8c520c717523b4671068e1a9
ruizh <ruizh@LAPTOP-NT1TJVTA>
2023-11-14 15:11:08 +0800

    Complete L05Q04 (THIS QUESTION IS SUPER DUPER HARDDDDD AHHHHHH)

--- 23004890/src/Week05/L05Q04.java
+++ 23004890/src/Week05/L05Q04.java
@@ -9,5 +9,37 @@ package Week05;
  * @author ruizh
  */
 public class L05Q04 {
-    
+    public static void main(String[] args) {
+        int[][] matrix = {{1,5,7},{3,6,9},{5,3,8}};
+        System.out.println("3 by 3 Matrix");
+        for(int counter = 0; counter < matrix.length; counter++){
+            for(int index = 0; index < matrix[counter].length; index++){
+                System.out.printf("%-3d",(matrix[counter][index]));
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+        System.out.println("After rotates 90 degrees clockwise");
+        int temp = 0;
+        for(int rows = 0; rows < matrix.length; rows++){
+            for(int columns = rows; columns < matrix.length; columns++){
+                temp = matrix[rows][columns];
+                matrix[rows][columns] = matrix[columns][rows]; 
+                matrix[columns][rows] = temp; 
+            }  
+        }
+        int temp2=0;
+        for(int rows = 0; rows < matrix.length; rows++){
+            temp2 = matrix[rows][0];
+            matrix[rows][0] = matrix[rows][2];
+            matrix[rows][2] = temp2;
+        }
+
+        for(int rows = 0; rows < matrix.length; rows++){
+            for(int columns = 0; columns < matrix[rows].length; columns++)
+            System.out.printf("%-3d",(matrix[rows][columns]));
+            System.out.println("");
+         }
+        
+    }
 }
