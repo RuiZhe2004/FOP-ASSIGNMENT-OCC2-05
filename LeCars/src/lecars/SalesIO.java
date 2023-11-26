53c301c820118beb2250dcaa405c1f0b0506cdd9
szeyu <szeyusim@gmail.com>
2023-11-26 13:46:02 +0800

    done for other io java

---
+++ LeCars/src/lecars/SalesIO.java
@@ -0,0 +1,41 @@
+package lecars;
+
+import lecars.StreamReaderHandler;
+import java.io.*;
+
+public class SalesIO {
+    public static void main(String[] args) {
+        getSalesInput();
+    }
+    
+    public static String[][] getSalesInput() {
+        String[][] sales = {{}};
+
+        // Get an InputStream for the resource file sales.csv
+        InputStream resourceStream = SalesIO.class.getResourceAsStream("/data/sales.csv");
+
+        if (resourceStream != null) {
+            // Convert the InputStream to a String
+            String fileContent = StreamReaderHandler.convertStreamToString(resourceStream);
+
+            // to remove the unused row
+            fileContent = fileContent.replaceFirst("salesId,dateTime,carPlate,custId,employeeId\\n", "");
+
+            // Print or process the file content as needed
+            System.out.println(fileContent);
+
+
+            String[] lineSplit = fileContent.split("\\n");
+            sales = new String[lineSplit.length][5];
+
+            for(int line = 0; line < lineSplit.length; line++){
+                // line - 1 to not include the index name
+                sales[line] = lineSplit[line].split(",");
+            }
+            
+        } else {
+            System.err.println("Resource file not found.");
+        }
+        return sales;        
+    }
+}
