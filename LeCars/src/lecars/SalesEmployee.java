927e18e5ecab27b19b8e58475f8e5dd8784877d6
ooiru <ooiru@ReztonRZ>
2023-11-27 22:55:05 +0800

    Solved SalesEmployee EmployeeID auto increment by rz_rexton

--- LeCars/src/lecars/SalesEmployee.java
+++ LeCars/src/lecars/SalesEmployee.java
@@ -10,46 +10,55 @@ package lecars;
  */
 
 import java.io.*;
+import java.util.List;
+import static lecars.EmployeeIO.getEmployeeInput;
+
 
 public class SalesEmployee extends EmployeeIO {
     public static void main(String[] args) {
         registerNewUser("test","test");
     }
-    public static void registerNewUser(String employeeName, String password){
-        //read and get the employee data
-        String[][] employee = getEmployeeInput();
-        
-        //Get the last row of EmployeeID
-        String lastEmpID = (employee[employee.length-1][0]).substring(1);
-        int intlastEmpID = Integer.parseInt(lastEmpID);  //Convert to int so that can +1
-        
-        // employeeID Auto Increment
-        intlastEmpID++;   //+1
-        String strlatestEmpID = Integer.toString(intlastEmpID); //Convert int back to string
-        String combinedlatestEmpID = ("0000"+intlastEmpID); 
-        String extractlast4digit = combinedlatestEmpID.substring(combinedlatestEmpID.length()-4); //extract the last 4 digit from the string
-        
-        //Get substring last 4 digit
-        String employeeID;
-        employeeID = "E"+extractlast4digit;
+    
+    public SalesEmployee(String employeeId, String employeeName, int employeeStatus, String password) {
+        super(employeeId, employeeName, employeeStatus, password); // Calls the superclass constructor
+    }
+    
+        public static void registerNewUser(String employeeName, String password) {
+        // Read and get the employee data
+        List<EmployeeIO> employees = getEmployeeInput();
 
-        // status always 0 as a sales employee
-        final String status = "0";
+        // Check if the list is empty or not
+        String employeeID;
+        if (!employees.isEmpty()) {
+            // Get the last employee's ID
+            String lastEmpID = employees.get(employees.size() - 1).getEmployeeId();
+            int intLastEmpID = Integer.parseInt(lastEmpID.substring(1)); // Increment the ID
+
+            // employeeID Auto Increment
+            intLastEmpID++;   //+1
+            String strlatestEmpID = Integer.toString(intLastEmpID); //Convert int back to string
+            String combinedlatestEmpID = ("0000"+intLastEmpID); 
+            String extractlast4digit = combinedlatestEmpID.substring(combinedlatestEmpID.length()-4); //extract the last 4 digit from the string
         
-        try(FileWriter fw = new FileWriter("src/data/employee.csv", true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter out = new PrintWriter(bw)) // give access to println (coder friendly)
-        {
+            //Get substring last 4 digit
+            employeeID = "E"+extractlast4digit;
+
+        // Status is always "0" as a sales employee
+        final String status = "0";
+
+        try (FileWriter fw = new FileWriter("src/data/employee.csv", true);
+             BufferedWriter bw = new BufferedWriter(fw);
+             PrintWriter out = new PrintWriter(bw)) {
+
             String lineOfData = employeeID + "," + employeeName + "," + status + "," + password;
             out.println(lineOfData);
-            
-            out.close();
+
             System.out.println("Successfully wrote to the file.");
-            
+
         } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+          System.out.println("An error occurred.");
+          e.printStackTrace();
+          }
         }
-        
     }
 }
