c7987050f9d5ba5580f0a8391e113f03c210020a
szeyu <szeyusim@gmail.com>
2023-11-27 08:04:31 +0800

    add object instance first

--- LeCars/src/lecars/SalesIO.java
+++ LeCars/src/lecars/SalesIO.java
@@ -2,8 +2,68 @@ package lecars;
 
 import lecars.StreamReaderHandler;
 import java.io.*;
+import java.time.LocalDateTime;
 
 public class SalesIO {
+    private String salesId;
+    private LocalDateTime dateTime;
+    private String carPlate;
+    private String custId;
+    private String employeeId;
+    
+    // Constructor
+    public SalesIO(String salesId, LocalDateTime dateTime, String carPlate, String custId, String employeeId) {
+        this.salesId = salesId;
+        this.dateTime = dateTime;
+        this.carPlate = carPlate;
+        this.custId = custId;
+        this.employeeId = employeeId;
+    }
+    
+    public String getSalesId() {
+        return salesId;
+    }
+
+    public LocalDateTime dateTime() {
+        return dateTime;
+    }
+
+    public String getCarPlate() {
+        return carPlate;
+    }
+
+    public String getCustId() {
+        return custId;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setSalesId(String salesId) {
+        this.salesId = salesId;
+    }
+    
+    public void setDateTime() {
+        this.dateTime = LocalDateTime.now();
+    }
+
+    public void setCarPlate(String carPlate) {
+        this.carPlate = carPlate;
+    }
+    
+    public void setCustId(String custId) {
+        this.custId = custId;
+    }
+    
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String toString(){
+        return salesId+","+dateTime+","+carPlate+","+custId+","+employeeId;
+    }
+    
     public static void main(String[] args) {
         getSalesInput();
     }
